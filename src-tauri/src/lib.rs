7ccfe32700f9e07cb1a67cde650e1fa8c8de9faa
auureli0 <142776989+auureli0@users.noreply.github.com>
2025-09-04 18:17:21 +0200

    feat: add Playground for testing

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,23 +1,32 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-use std::fs;
-use std::path::Path;
-use tauri::Manager;
+use std::{collections::HashMap, fs};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+struct PathFile {
+    name: String,
+    is_folder: bool,
+}
+
 #[tauri::command]
-fn list_files_dir() -> Result<Vec<String>, String> {
-    let mut files_list: Vec<String> = Vec::new();
+fn list_files_dir() -> Result<Vec<PathFile>, String> {
+    let mut files_list: Vec<PathFile> = Vec::new();
     
-    let files = fs::read_dir("./").map_err(|e| e.to_string())?;
+    let files = fs::read_dir("C:/Projects/st/Thuer/").map_err(|e| e.to_string())?;
     for file in files {
         if let Ok(de) = file {
             let file_name = de.file_name();
             let entry = file_name.to_string_lossy().into_owned();
-            files_list.push(entry.to_string());
+            
+            let path_file = PathFile {
+                name: entry,
+                is_folder: true,
+            };
+
+            files_list.push(path_file);
         }
     }
 
